326608d6a4b8aae6035bbb33417d1d79b6bc25e6
Pooja Unnikrishnan <100412366+PoojaUnnikrishnan@users.noreply.github.com>
2024-10-10 15:39:52 +0530

    Update auth.services.js

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -4,7 +4,7 @@
 //importing axios to make http request.
 
 import axios from "axios";
-const API_URL = "https://imgur--backend.herokuapp.com/";
+const API_URL = process.env.REACT_APP_API_URL
 
 //function to register details of user and post the details of user in database using API.
 const register = (fullName, email, password) => {
